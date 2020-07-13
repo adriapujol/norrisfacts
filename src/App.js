d3f120ac6b850cf6d1aba333202a8333daebc0b3
Adria Pujol <adriapujolaparicio@gmail.com>
2020-07-13 18:53:37 +0200

    first steps. Img working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Category from './Category';
+import norris from './img/chuck_norris_s.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h2 className="logo">Chuck Norris Facts</h2>
+          <nav className="navbar">
+            <Category />
+          </nav>
+        </header>
+        <section>
+          <img id="norris" src={norris} alt="Norris"></img>
+        </section>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/Category.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Category extends Component {
+    render() {
+        return (
+            <h1>hi dummy</h1>
+        )
+    }
+}
+
+
+export default Category;
